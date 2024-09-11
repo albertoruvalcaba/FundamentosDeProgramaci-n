683395899a5b2a54ed935e73c0c482d216035c83
alberto <l24121366@morelia.tecnm.mx>
2024-09-11 15:34:17 -0600

    tarea3

---
+++ Main.java
@@ -0,0 +1,42 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Sección 1: Información de un libro favorito
+        System.out.println("Información de mi libro favorito");
+        System.out.println("Título: Hábitos Atómicos");
+        System.out.println("Autor: James Clear");
+        System.out.println("Año de publicación: 2018");
+        System.out.println("Género: Autoayuda");
+        System.out.println("Número de páginas: 328");
+
+        // Sección 2: Información de un país que deseas visitar
+        String nombrePais = "España";
+        String capital = "Madrid";
+        String idiomaOficial = "Español";
+        double poblacionMillones = 47.78;
+        String moneda = "Euro";
+
+        System.out.println("Información del país que deseo visitar:");
+        System.out.println("Nombre del país: " + nombrePais);
+        System.out.println("Capital: " + capital);
+        System.out.println("Idioma oficial: " + idiomaOficial);
+        System.out.println("Población: " + poblacionMillones + "millones");
+        System.out.println("Moneda: " + moneda);
+
+        // Sección 3: Información de un personaje histórico favorito
+        String nombreCompleto = "Aristóteles";
+        String fechaNacimiento = "384 a.C.";
+        String nacionalidad = "Griega";
+        String ocupacion = "Filósofo";
+        String logroDestacado = "La Lógica";
+
+        System.out.println("Información del personaje histórico favorito:");
+        System.out.println("Nombre completo: " + nombreCompleto);
+        System.out.println("Fecha de nacimiento: " + fechaNacimiento);
+        System.out.println("Nacionalidad: " + nacionalidad);
+        System.out.println("Ocupación: " + ocupacion);
+        System.out.println("Logro destacado: " + logroDestacado);
+
+    }
+}
