395214bbfacb4e4929d87d7a5bad0de1244f1122
alberto <l24121366@morelia.tecnm.mx>
2024-09-11 23:08:10 -0600

    correcciones

--- tarea3unidad2.java
+++ tarea3unidad2.java
@@ -1,90 +1,42 @@
-import java.sql.SQLOutput;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Esta es una variable de tipo String (Cadena)
-        String nombre = "Alberto";
+        // Sección 1: Información de un libro favorito
+        System.out.println("Información de mi libro favorito");
+        System.out.println("Título: Hábitos Atómicos");
+        System.out.println("Autor: James Clear");
+        System.out.println("Año de publicación: 2018");
+        System.out.println("Género: Autoayuda");
+        System.out.println("Número de páginas: 328");
 
-        // Variable de tipo int (número)
-        int edad = 18;
+        // Sección 2: Información de un país que deseas visitar
+        String nombrePais = "España";
+        String capital = "Madrid";
+        String idiomaOficial = "Español";
+        double poblacionMillones = 47.78;
+        String moneda = "Euro";
 
-        // Variable de tipo char (Caracter)
-        char genero = 'H';
+        System.out.println("Información del país que deseo visitar:");
+        System.out.println("Nombre del país: " + nombrePais);
+        System.out.println("Capital: " + capital);
+        System.out.println("Idioma oficial: " + idiomaOficial);
+        System.out.println("Población: " + poblacionMillones + "millones");
+        System.out.println("Moneda: " + moneda);
 
-        // Variable de tipo Double (decimal)
-        Double estatura = 1.88;
+        // Sección 3: Información de un personaje histórico favorito
+        String nombreCompleto = "Aristóteles";
+        String fechaNacimiento = "384 a.C.";
+        String nacionalidad = "Griega";
+        String ocupacion = "Filósofo";
+        String logroDestacado = "La Lógica";
 
-        // Variable de tipo boolean (boleana)
-        boolean esCasada = true;
-
-        System.out.println(nombre);
-        System.out.println(edad);
-        System.out.println(genero);
-        System.out.println(estatura);
-        System.out.println(esCasada);
-
-        /*
-        CONCATENACIÓN (Unir cosas; variables, texto, etc)
-         */
-
-        System.out.println("Mi nombre es: " + nombre);
-        System.out.println("Mi edad es: " + edad);
-        System.out.println("Mi genero es: " + genero);
-        System.out.println("Mi estatura es: " + estatura);
-        System.out.println("Mi esCasada es: " + esCasada);
-
-        /*
-        Organizar su código en 3 secciones divididas por comentarios
-
-        - Sección 1: Información de un familiar
-        - Mostrar:
-            Nombre, edad, genero, año de nacimiento y estatura
-
-        - Sección 2: Información de su mascota
-        - Mostrar:
-            Nombre, edad, raza, color y sexo
-
-        -Sección 3: Información de su pelicula favorita
-        -Mostrar:
-           Nombre, año de lanzamiento, duración, genero, director
-
-        En la sección 2 y 3 almacenar los datos en variables en la  primera poner
-        los datos directamente en el "print"
-        */
-        // Sección 1
-        System.out.println("Nombre Familiar: Simon");
-        System.out.println("Edad: 22 años");
-        System.out.println("Género: M");
-        System.out.println("Año de Nacimiento: 2002");
-        System.out.println("Estatura: 1.80m");
-
-        //Sección 2
-        String nombreMascota = "Caguamo";
-        int edadMascota = 2;
-        String razaMascota = "Cane Corso";
-        String colorMascota = "Negro";
-        char sexoMascota = 'M';
-
-        System.out.println("Nombre de la mascota: " + nombreMascota);
-        System.out.println("Edad: " + edadMascota + " años");
-        System.out.println("Raza: " + razaMascota);
-        System.out.println("Color: " + colorMascota);
-        System.out.println("Sexo: " + sexoMascota);
-
-        //Sección 3
-        String nombrePelicula = "Interestelar";
-        int añoDeLanzamiento = 2014;
-        Double duracionPelicula = 2.49;
-        String generoPelicula = "Ciencia ficción";
-        String directorPelicula = "Christopher Nolan";
-
-        System.out.println("Película: " + nombrePelicula);
-        System.out.println("Lanzamiento: " + añoDeLanzamiento);
-        System.out.println("Duración: " + duracionPelicula + " horas");
-        System.out.println("Género: " + generoPelicula);
-        System.out.println("Director: " + directorPelicula);
+        System.out.println("Información del personaje histórico favorito:");
+        System.out.println("Nombre completo: " + nombreCompleto);
+        System.out.println("Fecha de nacimiento: " + fechaNacimiento);
+        System.out.println("Nacionalidad: " + nacionalidad);
+        System.out.println("Ocupación: " + ocupacion);
+        System.out.println("Logro destacado: " + logroDestacado);
 
     }
 }
